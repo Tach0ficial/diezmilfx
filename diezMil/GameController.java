f3b4320e8702cdf5ae06d895775787b1bf7dfeb6
Tach0ficial <48481694+Tach0ficial@users.noreply.github.com>
2021-05-15 21:46:56 +0200

    Transición de imagenes y fix bug de la transición.

--- diezMil/GameController.java
+++ diezMil/GameController.java
@@ -1,6 +1,9 @@
 package diezMil;
 
+
+import javafx.util.Duration;
 import java.util.ArrayList;
+import java.util.Iterator;
 import diezMil.game.Game;
 import diezMil.game.Player;
 import javafx.fxml.FXML;
@@ -9,9 +12,15 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.animation.ParallelTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Transition;
 import javafx.event.ActionEvent;
 
 public class GameController {
@@ -19,10 +28,12 @@ public class GameController {
   private Game game;
   private int currentPlayer = 0;
   private boolean isLastPlayer = false;
+  private ImageView[] diceImage = new ImageView[6];
 
   public final void setGame(ArrayList<Player> players) {
     this.game = new Game(players);
     PlayerName.setText(game.getPlayers().get(0).getName());
+    updatePlayer();
   }
 
   @FXML
@@ -44,42 +55,106 @@ public class GameController {
   private TableColumn<?, ?> tablePointsColunm;
 
   @FXML
-  private Pane centro;
+  private Text TotalPlayerPoints;
 
   @FXML
-  private Button rollDiceButton;
+  private VBox centro;
+
+  @FXML
+  private Button btnShowTable;
+
+  @FXML
+  private Button btnMute;
+
+  @FXML
+  private ImageView die1;
+
+  @FXML
+  private ImageView die2;
+
+  @FXML
+  private ImageView die3;
+
+  @FXML
+  private ImageView die4;
+
+  @FXML
+  private ImageView die5;
+
+  @FXML
+  private ImageView die6;
 
   @FXML
   private TextField currentPoints;
 
   @FXML
-  private TextArea visorDados;
-
+  private Button rollDiceButton;
 
   @FXML
-  private ImageView derecha;
+  private VBox derecha;
+
+  @FXML
+  private Button skipBtn;
+
+
 
   @FXML
   private void initialize() {
-    PlayerPoints.setText("0");
+
+    diceImage[0] = die1;
+    diceImage[1] = die2;
+    diceImage[2] = die3;
+    diceImage[3] = die4;
+    diceImage[4] = die5;
+    diceImage[5] = die6;
   }
 
   @FXML
   void rollDiceClick(ActionEvent event) throws InterruptedException {
-    if (currentPlayer+1 == game.getPlayers().size()) {
+
+    setImageDiceDefault();
+
+    if (getCurrentPlayer().getRoundPoints().get(game.getRound() - 1) != 0) {
+      game.getDiceCup().removeDice();
+    } else {
+      game.getDiceCup().reset();
+    }
+
+    if (currentPlayer + 1 == game.getPlayers().size()) {
       isLastPlayer = true;
     } else {
       isLastPlayer = false;
     }
 
     game.getDiceCup().throwDice();
+
+    ParallelTransition trans = new ParallelTransition();
+
+    for (int i = 0; i < game.getDiceCup().getDice().size(); i++) {
+      diceImage[i]
+          .setImage(new Image(getClass().getResourceAsStream("images/die_roll_animation.gif")));
+      diceImage[i].setDisable(true);
+
+      double pause = 0.5 + Math.random();
+      int diceIndex = i;
+
+      PauseTransition delay = new PauseTransition(Duration.seconds(pause));
+      delay.setOnFinished(event1 -> updateDiceImage(diceIndex));
+      trans.getChildren().add(delay);
+
+    }
+
+    trans.play();
+    System.out.println("Ronda: " + game.getRound());
+    System.out.println(getCurrentPlayer().getName());
+    System.out.println("Puntos: " + getCurrentPlayer().getRoundPoints().get(game.getRound() - 1));
     game.getDiceCup().printDice();
     int countedPoints = game.getDiceCup().countPoints();
     currentPoints.setText(countedPoints + "");
     if (countedPoints == 0) {
       getCurrentPlayer().getRoundPoints().set(game.getRound() - 1, 0);
       showRoundPoints();
-      Thread.sleep(3000);
+      // Thread.sleep(3000);
       nextPlayer();
     } else {
       addPoints(countedPoints);
@@ -97,26 +172,31 @@ public class GameController {
           System.out.println("Todos han perdido.");
         }
       }
-      Thread.sleep(3000);
-      if (game.getDiceCup().getDice().isEmpty()) {
+
+      if (game.getDiceCup().getDice().size() == game.getDiceCup().diceToRemove()) {
+        // Thread.sleep(3000);
         nextPlayer();
       } else {
-        
-        /* TODO - Preguntar si quiere tirar de nuevo.
-        if() {
-          
-        }else {
-          nextPlayer();
-        }
-        */
+
+        /*
+         * TODO - Preguntar si quiere tirar de nuevo. if() {
+         * 
+         * }else { nextPlayer(); }
+         */
       }
     }
     updateTable();
   }
 
+  private void setImageDiceDefault() {
+    for (int i = 0; i < diceImage.length; i++) {
+      diceImage[i].setImage(new Image(getClass().getResourceAsStream("images/die_selected.png")));
+    }
+  }
+
   private void updateTable() {
     // TODO Actualizar la tabla de la izquierda.
-    
+
   }
 
   private void showRoundPoints() {
@@ -124,17 +204,28 @@ public class GameController {
   }
 
   private void nextPlayer() {
+
     if (isLastPlayer) {
       currentPlayer = 0;
       game.setRound(game.getRound() + 1);
     } else {
       currentPlayer++;
     }
+    getCurrentPlayer().getRoundPoints().add(game.getRound() - 1, 0);
+
+    ParallelTransition trans = new ParallelTransition();
+
+    PauseTransition delay = new PauseTransition(Duration.seconds(4));
+    delay.setOnFinished(event1 -> updatePlayer());
+    trans.getChildren().add(delay);
+    trans.play();
+  }
+
+  private void updatePlayer() {
+    setImageDiceDefault();
     currentPoints.clear();
     PlayerPoints.setText("0");
     PlayerName.setText(getCurrentPlayer().getName());
-    game.getDiceCup().reset();
-    getCurrentPlayer().getRoundPoints().add(game.getRound() - 1, 0);
   }
 
   private Player getCurrentPlayer() {
@@ -151,6 +242,35 @@ public class GameController {
 
   }
 
+  private void updateDiceImage(int index) {
+    int faceValue = game.getDiceCup().getDice().get(index).getValue();
+
+    switch (faceValue) {
+      case 1:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die1.png")));
+        break;
+      case 2:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die2.png")));
+        break;
+      case 3:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die3.png")));
+        break;
+      case 4:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die4.png")));
+        break;
+      case 5:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die5.png")));
+        break;
+      case 6:
+        diceImage[index].setImage(new Image(getClass().getResourceAsStream("images/die6.png")));
+        break;
+      default:
+        diceImage[index].setImage(null); // should never occur
+        break;
+    }
+
+    diceImage[index].setDisable(false);
+  }
 
 
 }
--- diezMil/game/DiceCup.java
+++ diezMil/game/DiceCup.java
@@ -1,8 +1,8 @@
 package diezMil.game;
 
 /**
- * Class DiceCup
- * Representación de cubilete
+ * Class DiceCup Representación de cubilete
+ * 
  * @author Carlos Hidalgo Risco y Laura Hidalgo Rivera
  */
 
@@ -19,18 +19,18 @@ public class DiceCup {
       this.dice.add(new Die());
     }
   }
-  
+
   public void reset() {
     this.dice.clear();
     for (int i = 0; i < 6; i++) {
       this.dice.add(new Die());
     }
   }
-  
+
   public final ArrayList<Die> getDice() {
     return dice;
   }
-  
+
   /**
    * Tira los dados que haya en el cubilete
    */
@@ -39,7 +39,7 @@ public class DiceCup {
       die.throwDie();
     }
   }
-  
+
   /**
    * Imprime los dados en forma de dibujo
    */
@@ -56,15 +56,14 @@ public class DiceCup {
 
   /**
    * Cuenta los puntos de una tirada y borra los dados que dan puntos
+   * 
    * @return points
    */
   public int countPoints() {
-    /* Este mapa lo utilizamos para relacionar la cantidad de dados de un mismo valor
-     * con el multiplicador de puntos, de manera que:
-     * cantidad:      3, 4, 5, 6
-     * multiplicador: 1, 2, 4, 8
-     * Para el multiplicador:
-     * 2^0=1, 2^1=2, 2^2=4, 2^3=8
+    /*
+     * Este mapa lo utilizamos para relacionar la cantidad de dados de un mismo valor con el
+     * multiplicador de puntos, de manera que: cantidad: 3, 4, 5, 6 multiplicador: 1, 2, 4, 8 Para
+     * el multiplicador: 2^0=1, 2^1=2, 2^2=4, 2^3=8
      */
     Map<Integer, Integer> multi = new HashMap<>();
     for (int j = 0; j < 4; j++) {
@@ -74,7 +73,6 @@ public class DiceCup {
     int points = 0;
     if (this.dice.containsAll(new ArrayList<Die>(
         Arrays.asList(new Die(1), new Die(2), new Die(3), new Die(4), new Die(5), new Die(6))))) {
-      this.dice.clear();
       return 1500;
     } else {
       for (int i = 1; i <= 6; i++) {
@@ -96,12 +94,54 @@ public class DiceCup {
           points += count * 50;
         }
 
+      }
+    }
+    return points;
+  }
+
+  public void removeDice() {
+    /*
+     * Este mapa lo utilizamos para relacionar la cantidad de dados de un mismo valor con el
+     * multiplicador de puntos, de manera que: cantidad: 3, 4, 5, 6 multiplicador: 1, 2, 4, 8 Para
+     * el multiplicador: 2^0=1, 2^1=2, 2^2=4, 2^3=8
+     */
+
+    if (this.dice.containsAll(new ArrayList<Die>(
+        Arrays.asList(new Die(1), new Die(2), new Die(3), new Die(4), new Die(5), new Die(6))))) {
+      this.dice.clear();
+    } else {
+      for (int i = 1; i <= 6; i++) {
+        int side = i;
+
+        // cuenta los dados con el mismo valor
+        int count = (int) this.dice.stream().filter(die -> die.getValue() == side).count();
+
         // borra los dados que puntuan
         this.dice.removeIf(die -> die.getValue() == side
             && (count >= 3 || (die.getValue() == 1 || die.getValue() == 5)));
       }
     }
-    return points;
   }
+  
+  public int diceToRemove() {
+    int diceToRemove = 0;
+    if (this.dice.containsAll(new ArrayList<Die>(
+        Arrays.asList(new Die(1), new Die(2), new Die(3), new Die(4), new Die(5), new Die(6))))) {
+      return 6;
+    } else {
+      for (int i = 1; i <= 6; i++) {
+        int side = i;
+
+        // cuenta los dados con el mismo valor
+        int count = (int) this.dice.stream().filter(die -> die.getValue() == side).count();
+        
+        if (side == 1 || side == 5 || count >= 3) {
+          diceToRemove += count;
+        }    
+      }
+      return diceToRemove;
+    }
+  }
+  
 
 }
